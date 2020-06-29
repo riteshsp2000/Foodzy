c9e90be748fbbf5787af9a9662caa19e48d5e73a
Ritesh Patil <riteshsp2000@gmail.com>
2020-06-29 14:34:47 +0530

    Added conditionals to check if the user exists. If not then create a user.

--- services/passport.js
+++ services/passport.js
@@ -18,12 +18,28 @@ passport.use(
       callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({
-        googleId: profile.id,
-        displayName: profile.displayName,
-        emails: profile.emails,
-        photos: profile.photos[0].value,
-      }).save();
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          // Conditional to check if the user exists.
+          // The done callback needs 2 arguments - error and the user record
+          done(null, existingUser);
+        } else {
+          // If no user exists then create one.
+          new User({
+            googleId: profile.id,
+            displayName: profile.displayName,
+            emails: profile.emails,
+            photos: profile.photos[0].value,
+          })
+            .save()
+            .then((user) => done(null, user));
+          // By using new User we get a new instance there. Along
+          // with that we alse get a instance in the then callback.
+          // By convention we use the instance in the then callback as
+          // it is the latest one and has all the changes that took place while
+          // saving the user. The later one might not have it.
+        }
+      });
     }
   )
 );
