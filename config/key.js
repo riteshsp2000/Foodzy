0d7e4d31f2ed6637419c204d1a68b256e91e5c2e
Ritesh Patil <riteshsp2000@gmail.com>
2020-06-29 17:04:22 +0530

    Managed the keys

--- config/key.js
+++ config/key.js
@@ -1,10 +1,7 @@
-module.exports = {
-  googleClientID:
-    '352289078495-eu6vm7k10e9srggvqtqeq5dbv1a4o68h.apps.googleusercontent.com',
-  googleClientSecret: 'UeUmQSW17P4mO7gzKMjYyzva',
-  mongoURI:
-    'mongodb+srv://riteshsp2000:9tb37dyeL5cPNDbL@cluster0-1vrv9.mongodb.net/<dbname>?retryWrites=true&w=majority',
-  cookieKey: 'akfjalskdjflksdjl;fkajsdlfkajflka;kjdaslkfjadlkfjljasdlfkjd;l',
-};
+// Configure the file to figure out which set of credentials to return ie dev or prod
 
-// 9tb37dyeL5cPNDbL
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./prod');
+} else {
+  module.exports = require('./dev');
+}
