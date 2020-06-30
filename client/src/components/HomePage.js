8f61c67cf8e9fe58ad93b2804ae77c7d85b657fa
Ritesh Patil <riteshsp2000@gmail.com>
2020-07-01 00:47:19 +0530

    Added typed interface on ho home page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Typed from 'react-typed';
 
 const HomePage = () => {
   return (
     <header className='heading-container'>
-      <div className='heading-image'></div>
+      <div className='heading-image'>
+        <Typed
+          strings={[
+            'Hungry? Feeling Lazy?',
+            "Don't want to leave the bed?",
+            'In the middle of a film?',
+            'No worries! Make a request!',
+            'And get your food at your place!',
+          ]}
+          typeSpeed={40}
+          loop={true}
+          className='typed-intro'
+        />
+      </div>
       <h1>dfajsdlkfjlk</h1>
       <h1>dfajsdlkfjlk</h1>
       <h1>dfajsdlkfjlk</h1>
