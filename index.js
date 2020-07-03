fea7a04928a2381599d76fdda7421976225b6d1d
Ritesh Patil <riteshsp2000@gmail.com>
2020-07-03 19:18:54 +0530

    Configured the server to serve static files and additional changes so that react runs well

--- index.js
+++ index.js
@@ -25,5 +25,17 @@ app.use(passport.session());
 // Importing all the routes
 require('./routes/authRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // making sure that express will serve up production assets
+  // like our main.js, main.css files
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file if it doesnt recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
