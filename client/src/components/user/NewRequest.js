0273faf6d2570670084436c6ade55361cee3a695
Ritesh Patil <riteshsp2000@gmail.com>
2020-07-05 02:27:29 +0530

    Added the review request page

--- client/src/components/user/NewRequest.js
+++ client/src/components/user/NewRequest.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 
 import Layout from './Layout';
 import RequestForm from './RequestForm';
+import RequestReview from './RequestReview';
 
 class NewRequest extends Component {
+  state = { showFormReview: false };
+
+  renderContent() {
+    if (this.state.showFormReview) {
+      return <RequestReview />;
+    }
+
+    return (
+      <RequestForm
+        onSurveySubmit={() => this.setState({ showFormReview: true })}
+      />
+    );
+  }
+
   render() {
     return (
       <Layout>
         <h2>New Request</h2>
-        <RequestForm />
+        {this.renderContent()}
       </Layout>
     );
   }
---
+++ client/src/components/user/RequestReview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RequestReview = () => {
+  return (
+    <div>
+      <h4>Request Review</h4>
+    </div>
+  );
+};
+
+export default RequestReview;
