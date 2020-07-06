de8eb3474fb47c92dea66adcc50ec2bdac441f64
Ritesh Patil <riteshsp2000@gmail.com>
2020-07-06 20:53:20 +0530

    Minor changes

--- client/src/components/ViewRequests.js
+++ client/src/components/ViewRequests.js
@@ -3,22 +3,36 @@ import { connect } from 'react-redux';
 
 import Layout from './user/Layout';
 import { fetchRequests } from '../actions/index';
+import ViewRequestCard from './ViewRequestCard';
 
 const ViewRequests = ({ requests, fetchRequests }) => {
   useEffect(() => {
     fetchRequests();
   }, []);
 
+  const renderRequestCards = (requests) => {
+    switch (requests.length) {
+      case 0:
+        return 'Loading...';
+      default:
+        return requests.map((request, index) => {
+          return <ViewRequestCard key={index} items={request.items} />;
+        });
+    }
+  };
+
   return (
     <Layout>
       <h2>View Requests</h2>
-      <div className='requests-view-container'></div>
+      <div className='requests-view-container'>
+        {renderRequestCards(requests)}
+      </div>
     </Layout>
   );
 };
 
 const mapStateToProps = ({ requests }) => {
-  return requests;
+  return { requests };
 };
 
 export default connect(mapStateToProps, { fetchRequests })(ViewRequests);
