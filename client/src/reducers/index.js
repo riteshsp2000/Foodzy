45520a9e3a3563eb565778cb36d4e334b9318b85
Ritesh Patil <riteshsp2000@gmail.com>
2020-07-06 13:09:11 +0530

    create basic structure for the profile page

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form';
 
 import authReducer from './authReducer';
+import requestsReducers from './requestsReducer';
 
 export default combineReducers({
   auth: authReducer,
   form: reduxForm,
+  requests: requestsReducers,
 });
